2b6f432712cb80ad9678cc04e10ca8fcd00c6f70
许弘毅 <xuhongyi@dailyyoga.com>
2021-11-17 16:16:54 +0800

    项目初始化

---
+++ component/init.go
@@ -0,0 +1,30 @@
+package component
+
+import (
+	"sync"
+
+	"github.com/xhyonline/xutil/kv"
+
+)
+
+// Server 组件服务
+type Server struct {
+	Redis *kv.RClient
+}
+
+var (
+	Instance *Server
+	once     sync.Once
+)
+
+type Option func()
+
+// Init 初始化组建服务
+func Init(options ...Option) {
+	once.Do(func() {
+		Instance = new(Server)
+		for _, f := range options {
+			f()
+		}
+	})
+}
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/xhyonline/xdq/configs"
+	"github.com/xhyonline/xdq/internal"
+	"github.com/xhyonline/xdq/middleware"
+	"github.com/xhyonline/xdq/router"
+
+	"github.com/xhyonline/xutil/sig"
+
+
+	"github.com/xhyonline/xdq/component"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	g := gin.Default()
+	// 初始化配置
+	configs.Init(configs.WithRedis())
+	// 初始化 mysql 、redis 等服务组件
+	component.Init(component.RegisterRedis())
+	// 中间件
+	g.Use(middleware.Cors())
+	// 初始化路由
+	router.InitRouter(g)
+	// 启动 HTTP 服务
+	httpServer := &internal.HTTPServer{Server: &http.Server{Addr: ":8080", Handler: g}}
+	go httpServer.Run()
+	// 注册优雅退出
+	ctx := sig.Get().RegisterClose(httpServer)
+	<-ctx.Done()
+}
